eee3a45318835381f9be066ba6c4149a769a6d3b
Rich Abdill <richard.abdill@gmail.com>
2020-11-17 14:37:16 -0500

    typo

--- process_forwards.R
+++ process_forwards.R
@@ -65,7 +65,7 @@ seqtab <- makeSequenceTable(dada_forward)
 
 # Get rid of really short sequences that can't practically be used
 # to assign taxonomy:
-seqtab.noshort <- seqtab[,nchar(colnames(seqtab)) > 49])
+seqtab.noshort <- seqtab[,nchar(colnames(seqtab)) > 49]
 log(paste('Removed',length(colnames(seqtab) - length(colnames(seqtab.noshort)),'ASVs for being too short.'))
 
 # check for chimeras
