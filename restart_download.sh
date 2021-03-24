46f0d86d67d78f59bae53c085a6bd373c4d0016e
Rich Abdill <richard.abdill@gmail.com>
2021-03-23 20:24:42 -0400

    Typo

--- restart_download.sh
+++ restart_download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/run_restart_download.slurm
+sbatch --export=PROJECT=$1 --job-name=${1}redownload -o ${1}redownload.log ../code/run_restart_download.slurm
