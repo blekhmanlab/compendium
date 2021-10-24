b96d5c0309b58fb675e50afbc89522e2751443e8
Rich Abdill <richard.abdill@gmail.com>
2021-10-24 17:45:59 -0400

    Just overwrite download log

--- restart_download.sh
+++ restart_download.sh
@@ -2,4 +2,4 @@
 # NOTE: This should be run from within the logs directory.
 cd /home/blekhman/shared/compendium/logs
 
-sbatch --export=PROJECT=$1 --job-name=${1}redownload -o ${1}redownload.log ../code/run_restart_download.slurm
+sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/run_restart_download.slurm
