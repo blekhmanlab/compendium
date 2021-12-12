a9ca6a0042b159b51f97483d11d73f9a8fe7916b
Rich Abdill <richard.abdill@gmail.com>
2021-12-12 15:11:17 -0500

    Integrating fasterq yay

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/blekhman/shared/compendium/logs
-sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/run_download.slurm
+sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/run_fasterq.slurm
