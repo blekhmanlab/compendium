0ca3df9061297712ce11d1e684b2ca8e0a8c7f10
Rich Abdill <richard.abdill@gmail.com>
2021-12-20 14:41:53 -0500

    Removing old DL scripts, shrinking mem footprint

--- restart_download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# NOTE: This should be run from within the logs directory.
-cd /home/blekhman/shared/compendium/logs
-
-sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/run_restart_download.slurm
--- restart_download_unpack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/blekhman/shared/compendium/logs
-sbatch --export=PROJECT=$1 --job-name=${1}download -o ${1}download.log ../code/download_unpack.slurm
