27e11ac1263284f656d7ef912652b891c0c394b7
Rich Abdill <richard.abdill@gmail.com>
2021-12-26 20:07:54 -0500

    Clean out old results

--- restart_dada.sh
+++ restart_dada.sh
@@ -39,6 +39,7 @@ cat dada.template >> $filename
 rm /scratch.global/rabdill/bulk/${1}/results/*
 rm /scratch.global/rabdill/bulk/${1}/temp/*
 rm /scratch.global/rabdill/bulk/${1}/intermediate/*
+rm -rf /home/blekhman/shared/compendium/results/${1}
 
 cd /home/blekhman/shared/compendium/logs
 sbatch --export=PROJECT=$1 --job-name=${1}dada -o ${1}dada.log ../code/${filename}
