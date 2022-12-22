fceb4b575cfdfb2a964a7791ec0624899de7fbba
Richard Abdill <rabdill@umn.edu>
2022-12-22 15:31:13 -0500

    Better summary parsing

--- config_template.py
+++ config_template.py
@@ -2,12 +2,36 @@ Tool='compendium'
 Email='email_goes_here'
 Key='12345'
 
-# When evaluating the percentage of reads removed from a sample,
-# what thresholds should be used for the "warning" and "error" levels?
-chimera_worrisome = 10
-chimera_too_high = 60
 
-# When evaluating the percentage of forward reads removed from a
+############### Sample-level settings ###############
+#
+# When evaluating the proportion of reads removed from a sample,
+# what thresholds should be used for the "warning" and "error" levels?
+chimera_worrisome = 0.10
+chimera_error = 0.60 # MAXIMUM
+#
+# When evaluating the proportion of forward reads removed from a
 # sample, what thresholds should be used for the "warning" and "error" levels?
-merged_worrisome = 65
-merged_too_low = 50
+merged_worrisome = 0.65
+merged_error = 0.50 # MINIMUM
+##############################################################
+
+
+############### Project-level settings ###############
+#
+# When evaluating the proportion of SAMPLES flagged for excessive
+# chimeric reads, what thresholds should be used for determining whether
+# a project should be reprocessed?
+# (IMPORTANT: The thresholds for chimeras and merged reads work slightly differently
+# than the sample-level ones above: There is no "warning" level for projects.
+# The "worrisome" threshold here is *what proportion of samples must be marked as
+# worrisome for the project to be discarded*. The "error" threshold is what proportion
+# of samples must be marked as errors for the project to be discarded.)
+project_chimera_worrisome = 0.4
+project_chimera_error = 0.2
+#
+# When evaluating the proportion of samples flagged for low
+# rates of merged reads, what thresholds should be used?
+project_merged_worrisome = 0.4
+project_merged_error = 0.15
+##############################################################
--- summary_stats.py
+++ summary_stats.py
@@ -4,10 +4,58 @@ class Project:
     def __init__(self, name, samples):
         self.id = name
         self.samples = samples
+        self.sample_count = len(self.samples)
+
+        self._check_chimera()
+        self._check_merged()
+        self._evaluate_flags()
+
+    def _check_chimera(self):
+        """Determines what proportion of samples have excessive
+        chimeric reads"""
+        self.chimeric_warn = 0
+        self.chimeric_error = 0
+        for sample in self.samples:
+            if sample.chimeric_warn:
+                self.chimeric_warn += 1
+            if sample.chimeric_error:
+                self.chimeric_error += 1
+
+    def _check_merged(self):
+        """Determines what proportion of samples had an unacceptably
+        low proportion of reads merged"""
+        merged_warn = 0
+        merged_error = 0
+        self.paired = True
+
+        for sample in self.samples:
+            if not sample.is_paired:
+                self.paired = False
+                self.merged_warn = None
+                self.merged_error = None
+                break
+
+            if sample.merged_warn:
+                self.merged_warn += 1
+            if sample.merged_error:
+                self.merged_error += 1
+        self.merged_warn = merged_warn / len(self.samples)
+        self.merged_error = merged_error / len(self.samples)
+
+    def _evaluate_flags(self):
+        """Checks project stats against configured thresholds."""
+        self.merged_warn_rate = self.merged_warn / len(self.samples)
+        self.merged_error_rate = self.merged_error / len(self.samples)
+
+    def __repr__(self):
+        return f'(Project {self.id})'
+    def __str__(self):
+        return f'Project {self.id}'
+
 
 class Sample:
     def __init__(self, data):
-        self.srr = data.get('srr')
+        self.srr = data.get('srr')[:-8]
         self.input = int(data.get('dinput'))
         self.filter = int(data.get('filter'))
         self.forward = int(data.get('forwd'))
@@ -20,20 +68,25 @@ class Sample:
             self.reverse = int(data.get('revse'))
             self.merged = int(data.get('merged'))
             self._check_merged()
-            
+
     def _check_chimera(self):
         """Calculates the proportion of chimeric reads found
         in the reads that were not lost in some other way."""
         self.chimera_percent = 1-(self.nonchim / self.length_filter)
-        self.chimeric_warning = self.chimera_percent > config.chimera_worrisome
-        self.chimeric_error = self.chimera_percent > config.chimera_too_high
-    
+        self.chimeric_warn = self.chimera_percent > config.chimera_worrisome
+        self.chimeric_error = self.chimera_percent > config.chimera_error
+
     def _check_merged(self):
         """Calculates the proportion of forward reads that were merged
         with a reverse read."""
         self.merged_percent = self.merged / self.forward
-        self.merged_warning = self.merged_percent < config.merged_worrisome
-        self.merged_error = self.merged_percent < config.merged_too_low
+        self.merged_warn = self.merged_percent < config.merged_worrisome
+        self.merged_error = self.merged_percent < config.merged_error
+
+    def __repr__(self):
+        return f'(Sample {self.srr})'
+    def __str__(self):
+        return f'Sample {self.srr}'
 
 
 def load_summary(project):
@@ -58,4 +111,3 @@ def load_summary(project):
 def Process_summary(project):
     proj = Project(project, load_summary(project))
     print(proj.samples)
-    
