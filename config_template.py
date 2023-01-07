f5c1fa5fa3a72a95d61033dba97081fe92003795
Richard Abdill <rabdill@umn.edu>
2023-01-06 22:27:36 -0500

    New config values

--- config_template.py
+++ config_template.py
@@ -6,6 +6,10 @@ Key='12345'
 snakemake_git = 'git@github.com:blekhmanlab/snakemake-compendium.git'
 
 ############### Sample-level settings ###############
+# When evaluating the proportion of reads retained through the entire pipeline,
+# what thresholds should be used for the "warning" and "error" levels?
+retained_worrisome = 0.69
+retained_error = 0.59 # MINIMUM
 #
 # When evaluating the proportion of reads removed from a sample,
 # what thresholds should be used for the "warning" and "error" levels?
@@ -21,19 +25,23 @@ merged_error = 0.50 # MINIMUM
 
 ############### Project-level settings ###############
 #
-# When evaluating the proportion of SAMPLES flagged for excessive
-# chimeric reads, what thresholds should be used for determining whether
-# a project should be reprocessed?
-# (IMPORTANT: The thresholds for chimeras and merged reads work slightly differently
+# When evaluating the proportion of SAMPLES flagged for issues,
+# what thresholds should be used for determining whether a project
+# should be reprocessed?
+#
+# (IMPORTANT: The thresholds here work slightly differently
 # than the sample-level ones above: There is no "warning" level for projects.
 # The "worrisome" threshold here is *what proportion of samples must be marked as
 # worrisome for the project to be discarded*. The "error" threshold is what proportion
 # of samples must be marked as errors for the project to be discarded.)
-project_chimera_worrisome = 0.4
-project_chimera_error = 0.2
-#
-# When evaluating the proportion of samples flagged for low
-# rates of merged reads, what thresholds should be used?
+project_retained_worrisome = 0.70
+project_retained_error = 0.20
+
 project_merged_worrisome = 0.4
 project_merged_error = 0.15
+
+project_chimera_worrisome = 0.4
+project_chimera_error = 0.2
+
+
 ##############################################################
