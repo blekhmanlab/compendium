843a5804e5752b62c3780c820c4e79cdcab3d892
Rich Abdill <rabdill@uchicago.edu>
2025-03-13 00:05:23 -0400

    Minor logging

--- setup_snakemake.sh
+++ setup_snakemake.sh
@@ -3,18 +3,21 @@
 # project ID
 gotID=$(echo -n "$1" | wc -c) # count how long this string is
 if [ $gotID -lt 5 ]; then
+    echo "ProjectID parameter not long enough"
     exit 1
 fi
 
 cd "$1"
 
 if [ ! -d "./venv" ]; then
+    echo "Setting up virtalenv"
     python -m venv venv
 fi
 
 source venv/bin/activate
 
 if [ ! -f "./venv/bin/snakemake" ]; then
+    echo "Running pip"
     pip install --upgrade pip
     pip install -r requirements.txt
 fi
